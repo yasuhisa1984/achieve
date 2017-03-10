32836aa0cac0de0c8acc59b5d4aafdb91d7b3e7a
yasuhisa <penmouse275@nifty.com>
2017-03-10 12:01:36 +0000

    コメント機能実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,48 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
+  # コメントを保存、投稿するためのアクションです。
+  def create
+    # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
+    @comment = current_user.comments.build(comment_params)
+    @blog = @comment.blog
+
+    # respond_toは、クライアントからの要求に応じてレスポンスのフォーマットを変更します。
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        # JS形式でレスポンスを返します。
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @blog = @comment.blog
+    @comment.destroy
+
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
+        # JS形式でレスポンスを返します。
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    # ストロングパラメーター
+    def comment_params
+      params.require(:comment).permit(:blog_id, :content)
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+end
